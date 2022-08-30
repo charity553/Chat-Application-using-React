f4be90eea51a7920bf9b8367f7f0a8cfd7f60e12
charity553 <57391531+charity553@users.noreply.github.com>
2022-08-30 13:28:33 +0300

    App

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,10 @@
+const ChatFeed = (props) => {
+            console.log(props);
+
+            return (
+                <div>
+                    ChatFeed Hello
+                </div>
+            );
+}
+export default ChatFeed;
