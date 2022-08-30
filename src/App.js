2bb11bec9ec4867d1ca2578965a422f6fd982bef
charity553 <57391531+charity553@users.noreply.github.com>
2022-08-30 14:53:02 +0300

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { ChatEngine} from 'react-chat-engine';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '33a006a6-20b6-42ca-93d6-cd1066088617';
 
 const App = () => {
-    return (
-        <ChatEngine
-                height="100vh"
-                projectID="33a006a6-20b6-42ca-93d6-cd1066088617"
-                userName="javascriptmastery"
-                userSecret="123123"
-                renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps} />}
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-        />
-    );
-}
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
 export default App;
